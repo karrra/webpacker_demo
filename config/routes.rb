2eef969f9bbe7e2e024576e4304cce19b15e27df
Karra <xp0729@gmail.com>
2019-06-26 11:46:08 +1000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :articles
+  end
 end
